59d8091bf30c9256c03861eb85768d2cda2bf3aa
viclito <berglin1998@gmail.com>
2025-11-08 14:05:17 +0530

    first commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion'
-import subhaImage from '../Assets/image.png'
+import subhaImage from '../Assets/image.jpg'
 
 const confettiColors = ['#FF5DA2', '#FFE066', '#7C4DFF', '#4DD0E1']
 
